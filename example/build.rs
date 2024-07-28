b8238571a4ede0f400adbe6e8cf457626d4e5d10
Swoorup Joshi <swooruptrader@gmail.com>
2024-07-28 13:52:03 +1000

    format examples

--- example/build.rs
+++ example/build.rs
@@ -1,31 +1,32 @@
 use miette::{IntoDiagnostic, Result};
+use wgsl_bindgen::qs::quote;
 use wgsl_bindgen::{
-    qs::quote, GlamWgslTypeMap, Regex, WgslBindgenOptionBuilder, WgslShaderSourceType,
-    WgslTypeSerializeStrategy,
+  GlamWgslTypeMap, Regex, WgslBindgenOptionBuilder, WgslShaderSourceType,
+  WgslTypeSerializeStrategy,
 };
 
 fn main() -> Result<()> {
-    WgslBindgenOptionBuilder::default()
-        .workspace_root("shaders")
-        .add_entry_point("shaders/testbed.wgsl")
-        .add_entry_point("shaders/triangle.wgsl")
-        .skip_hash_check(true)
-        .serialization_strategy(WgslTypeSerializeStrategy::Bytemuck)
-        .type_map(GlamWgslTypeMap)
-        .override_struct_field_type(
-            [("utils::types::VectorsU32", "a", quote!(crate::MyTwoU32))].map(Into::into),
-        )
-        .add_override_struct_mapping(("utils::types::Scalars", quote!(crate::MyScalars)))
-        .add_custom_padding_field_regexp(Regex::new("_pad.*").unwrap())
-        .short_constructor(2)
-        .shader_source_type(
-            WgslShaderSourceType::UseComposerWithPath
-                | WgslShaderSourceType::UseComposerEmbed
-                | WgslShaderSourceType::UseEmbed,
-        )
-        .derive_serde(false)
-        .output("src/shader_bindings.rs")
-        .build()?
-        .generate()
-        .into_diagnostic()
+  WgslBindgenOptionBuilder::default()
+    .workspace_root("shaders")
+    .add_entry_point("shaders/testbed.wgsl")
+    .add_entry_point("shaders/triangle.wgsl")
+    .skip_hash_check(true)
+    .serialization_strategy(WgslTypeSerializeStrategy::Bytemuck)
+    .type_map(GlamWgslTypeMap)
+    .override_struct_field_type(
+      [("utils::types::VectorsU32", "a", quote!(crate::MyTwoU32))].map(Into::into),
+    )
+    .add_override_struct_mapping(("utils::types::Scalars", quote!(crate::MyScalars)))
+    .add_custom_padding_field_regexp(Regex::new("_pad.*").unwrap())
+    .short_constructor(2)
+    .shader_source_type(
+      WgslShaderSourceType::UseComposerWithPath
+        | WgslShaderSourceType::UseComposerEmbed
+        | WgslShaderSourceType::UseEmbed,
+    )
+    .derive_serde(false)
+    .output("src/shader_bindings.rs")
+    .build()?
+    .generate()
+    .into_diagnostic()
 }
